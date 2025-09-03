d4f2322d02e87ff1570e2240eeab2a02034a4545
Vince-Capin <vince.capin@synacy.com>
2025-09-04 02:30:16 +0800

    added working leave application

--- src/app/pages/manager/service/manager.service.ts
+++ src/app/pages/manager/service/manager.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import {LeaveRequest} from '../model/leave-application.interface';
 import {HttpClient} from '@angular/common/http';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class ManagerService {
   private URL = '/api/v1/leave/application'
 
   constructor(private readonly http: HttpClient) {
   }
 
-  createLeave(leaveRequest: LeaveRequest) {
+  applyLeave(leaveRequest: LeaveRequest) {
     return this.http.post(this.URL, leaveRequest);
   }
 }
