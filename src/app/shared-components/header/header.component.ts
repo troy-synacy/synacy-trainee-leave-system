41ab6c10c0827080a78517619d4a29ef42efd3d0
gelayyy66 <angela.villaester@synacy.com>
2025-09-05 11:12:47 +0800

    fixed: sidebar issue

--- src/app/shared-components/header/header.component.ts
+++ src/app/shared-components/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
